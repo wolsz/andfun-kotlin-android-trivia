08aac1d56c9e397036395845377c30fcfd33b26c
wolsz <wolsz1@gmail.com>
2020-11-11 20:33:16 -0500

    After first lesson

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -32,10 +32,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        // TODO (01) Find the navController from myNavHostFragment
+
         // Since we're using KTX, you can call this.findNavController
-        // TODO (02) Link the navController to our ActionBar
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
         // By calling NavigationUI.setupActionBarWithNavController
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 
     // TODO (03) Override onSupportNavigateUp
